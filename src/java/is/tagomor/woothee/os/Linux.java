4940a7d11475346bb90f6640d795ed05008e8460
TAGOMORI Satoshi <tagomoris@gmail.com>
2014-10-18 22:14:15 +0900

    fix for os_version of Android

--- src/java/is/tagomor/woothee/os/Linux.java
+++ src/java/is/tagomor/woothee/os/Linux.java
@@ -10,17 +10,29 @@ import is.tagomor.woothee.DataSet;
 import is.tagomor.woothee.Classifier;
 
 public class Linux extends AgentCategory {
+  private static Pattern androidOs = Pattern.compile("Android[- ](\\d+\\.\\d+(?:\\.\\d+)?)");
+
   public static boolean challenge(final String ua, final Map<String,String> result) {
     int pos = ua.indexOf("Linux");
     if (pos < 0) // not Linux
       return false;
 
     Map<String,String> data = DataSet.get("Linux");
-    if (ua.indexOf("Android") > -1)
+    String version = null;
+
+    if (ua.indexOf("Android") > -1) {
       data = DataSet.get("Android");
+      Matcher android = androidOs.matcher(ua);
+      if (android.find(pos)) {
+        version = android.group(1);
+      }
+    }
 
     updateCategory(result, data.get(DataSet.DATASET_KEY_CATEGORY));
     updateOs(result, data.get(DataSet.DATASET_KEY_NAME));
+    if (version != null) {
+      updateOsVersion(result, version);
+    }
     return true;
   }
 }
